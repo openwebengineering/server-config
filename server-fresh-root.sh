248c290540b4d0a38d37ea50870485a9ff5dd043
Ubuntu <ubuntu@domU-12-31-39-07-75-90.compute-1.internal>
2013-02-19 03:47:43 +0000

    Added server-{aptget,fresh,fresh-root}.sh

---
+++ server-fresh-root.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Steve Phillips / elimisteve
+# 2013.02.18
+
+# Install MongoDB
+apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" | tee -a /etc/apt/sources.list.d/10gen.list
+apt-get -y update
+apt-get -y install mongodb-10gen
+
+apt-get -y install virtualenv
+pip install virtualenvwrapper
---
+++ server-fresh.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Steve Phillips / elimisteve
+# 2013.02.18
+
+# Install Go
+cd ~
+wget http://go.googlecode.com/files/go1.0.3.linux-amd64.tar.gz
+tar xvf go1.0.3.linux-amd64.tar.gz
+echo '' >> ~/.bashrc
+echo 'export GOROOT=$HOME/go' >> ~/.bashrc
+echo 'export PATH=$PATH:$GOROOT/bin' >> ~/.bashrc
+source ~/.bashrc
+
